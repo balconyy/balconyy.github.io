8d66e68ed36ef0c74439af9eac4dea4561fa23da
Balcony <balcony.film@proton.me>
2026-05-16 15:54:32 +0000

    BLCN 1 - initial commit, architecture draft

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import MovieSearch from '@/features/search/components/MovieSearch.vue'
+</script>
+
+<template>
+  <header>
+  </header>
+
+  <main>
+    <MovieSearch />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
